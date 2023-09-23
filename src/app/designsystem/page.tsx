7f8a4d4642dc5a1c0baba6fc645bf7cf97f2c659
Seth Brown <sdbrown93@gmail.com>
2023-09-22 20:47:23 -0400

    design edits

--- src/app/designsystem/page.tsx
+++ src/app/designsystem/page.tsx
@@ -13,6 +13,17 @@ export default function Design() {
       <h5 className="text-three mt-10 tracking-widest text-2xl">
         <span className="text-gray-500 mr-3">03</span>INTERACTIVE ELEMENTS
       </h5>
+      <div className="navbar bg-gray-900 mt-10">
+        <ul className="flex text-center justify-evenly text-three">
+          <li className="cursor-pointer flex items-center h-16 active:border-b-4 active:pt-1">
+            00 ACTIVE
+          </li>
+          <li className="cursor-pointer h-16 flex items-center hover:border-b-4 hover:border-neutral-400 hover:pt-1">
+            01 HOVERED
+          </li>
+          <li className="cursor-pointer h-16 flex items-center">02 IDLE</li>
+        </ul>
+      </div>
     </div>
   );
 }
