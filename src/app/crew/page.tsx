01de201a5024ab02001b0327b310722a7f7dc3df
Seth Brown <sdbrown93@gmail.com>
2023-09-29 22:24:15 -0400

    crew updates

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -2,14 +2,70 @@
 
 import Navbar from "../components/Navbar";
 import "../styles/crew.css";
+import Image from "next/image";
+import data from "../data.json";
+import { useState } from "react";
 
 export default function Crew() {
+  const [crewIndex, setCrewIndex] = useState(0); // Initialize crew index state
+
+  const handleButtonClick = (index: number) => {
+    setCrewIndex(index); // Update crew index when a button is clicked
+  };
+
+  const currentCrewMember = data.crew[crewIndex];
+
+  const selectedImageSource = currentCrewMember?.images?.webp || "";
+
   return (
-    <>
-      <div className="background-container">
-        <Navbar />
-        <h1>C</h1>
+    <div className="background-container">
+      <Navbar />
+      <div className="flex flex-col items-center mt-8">
+        <div className="md:w-full md:pl-12">
+          <h5 className="text-three">
+            <span className="text-three/25 mr-3 font-extrabold">02</span>MEET
+            YOUR CREW
+          </h5>
+        </div>
+        <div className="w-full md:flex md:flex-col-reverse md:h-screen md:justify-between md:max-h-[1024px] lg:flex-row-reverse lg:justify-evenly">
+          <div className="flex justify-center mt-10 ">
+            <Image
+              src={selectedImageSource}
+              alt="crew member"
+              width={750}
+              height={750}
+              className="h-56 w-auto md:h-[525px]"
+            />
+          </div>
+          <div className="md:flex md:flex-col-reverse lg:h-[420px] lg:w-[600px]">
+            <div className="flex justify-center">
+              <hr className="h-px w-10/12 bg-three/25 border-0 md:hidden" />
+            </div>
+            <div className="my-8 flex justify-evenly w-40 m-auto">
+              {data.crew.map((crewMember, index) => (
+                <button
+                  key={index}
+                  className={`w-3.5 h-3.5 bg-three/25 rounded-full hover:bg-three/50 active:bg-three/100 focus:bg-three/100 ${
+                    index === crewIndex ? "bg-three/100" : "" // Apply active-button class if index matches
+                  }`}
+                  onClick={() => handleButtonClick(index)} // Set the click event to change crew index
+                ></button>
+              ))}
+            </div>
+            <div className="flex flex-col md:mt-10">
+              <h4 className="text-three/50 text-center md:my-2">
+                {currentCrewMember.role.toUpperCase()}
+              </h4>
+              <h3 className="text-three text-center">
+                {currentCrewMember.name.toUpperCase()}
+              </h3>
+              <p className="body-text text-secondary text-center px-10 mt-3 md:px-40">
+                {currentCrewMember.bio}
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -5,7 +5,6 @@ import "../styles/destination.css";
 import data from "../data.json";
 import Image from "next/image";
 import React, { useState } from "react";
-import moon from "../../assets/destination/image-moon.webp";
 
 export default function Destination() {
   // Select active li
@@ -30,54 +29,64 @@ export default function Destination() {
     <div className="background-container">
       <Navbar />
       <div className="flex flex-col items-center mt-8">
-        <h5 className="text-three">
-          <span className="text-three/25 mr-3 font-extrabold">01</span>PICK YOUR
-          DESTINATION
-        </h5>
-        <Image
-          src={selectedImageSource}
-          alt="planet image"
-          width={100}
-          height={100}
-          className="w-44 my-8"
-        />
-        <div>
-          <ul className="flex text-center justify-evenly text-secondary text-base gap-6">
-            {destinations.map((destination, index) => (
-              <li
-                key={index}
-                tabIndex={2}
-                className={`cursor-pointer flex items-start h-9 ${
-                  selectedDestination === destination
-                    ? "text-three border-b-4 border-three cursor-pointer flex items-start h-9 hover:border-b-4 hover:border-neutral-400 active:border-b-4 active:border-three active:text-three focus:border-b-4 focus:border-three focus:text-three"
-                    : "cursor-pointer flex items-start h-9 hover:border-b-4 hover:border-neutral-400 active:border-b-4 active:border-three active:text-three focus:border-b-4 focus:border-three focus:text-three"
-                }`}
-                onClick={() => handleDestinationChange(destination)}
-              >
-                {destination}
-              </li>
-            ))}
-          </ul>
+        <div className="md:w-full md:pl-12">
+          <h5 className="text-three">
+            <span className="text-three/25 mr-3 font-extrabold">01</span>PICK
+            YOUR DESTINATION
+          </h5>
         </div>
-        <h2 className="text-three my-3">
-          {selectedDestinationData?.name.toUpperCase()}
-        </h2>
-        <p className="body-text text-secondary text-center px-8">
-          {selectedDestinationData?.description}
-        </p>
-        <hr className="h-px w-10/12 my-6 bg-three/25 border-0" />
-        <div className="text-center flex flex-col">
-          <div className="mt-2">
-            <p className="subheading2 text-secondary">AVG. DISTANCE</p>
-            <p className="subheading1 text-three mt-2">
-              {selectedDestinationData?.distance.toUpperCase()}
-            </p>
+        <div className="w-full lg:flex lg:flex-row lg:justify-evenly lg:mt-24">
+          <div className="flex justify-center lg:w-[450px]">
+            <Image
+              src={selectedImageSource}
+              alt="planet image"
+              width={450}
+              height={450}
+              className="w-44 my-8 md:w-80 md:my-14 lg:w-full"
+            />
           </div>
-          <div className="my-8">
-            <p className="subheading2 text-secondary">EST. TRAVEL TIME</p>
-            <p className="subheading1 text-three mt-2">
-              {selectedDestinationData?.travel.toUpperCase()}
+          <div className="lg:w-[450px]">
+            <div className="px-16 md:px-48 lg:px-0">
+              <ul className="flex text-center justify-evenly lg:justify-start text-secondary text-base gap-6">
+                {destinations.map((destination, index) => (
+                  <li
+                    key={index}
+                    tabIndex={2}
+                    className={`cursor-pointer flex items-start h-9 ${
+                      selectedDestination === destination
+                        ? "text-three border-b-4 border-three cursor-pointer flex items-start h-9 hover:border-b-4 hover:border-neutral-400 active:border-b-4 active:border-three active:text-three focus:border-b-4 focus:border-three focus:text-three"
+                        : "cursor-pointer flex items-start h-9 hover:border-b-4 hover:border-neutral-400 active:border-b-4 active:border-three active:text-three focus:border-b-4 focus:border-three focus:text-three"
+                    }`}
+                    onClick={() => handleDestinationChange(destination)}
+                  >
+                    {destination}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <h2 className="text-three my-3 md:my-5 text-center lg:text-start">
+              {selectedDestinationData?.name.toUpperCase()}
+            </h2>
+            <p className="body-text text-secondary text-center px-8 md:px-20 lg:px-0 lg:text-start">
+              {selectedDestinationData?.description}
             </p>
+            <div className="flex justify-center">
+              <hr className="h-px w-10/12 my-6 bg-three/25 border-0 md:my-10 lg:w-full" />
+            </div>
+            <div className="text-center flex flex-col md:flex-row md:items-center md:justify-evenly md:w-full md:mb-12">
+              <div className="mt-2">
+                <p className="subheading2 text-secondary">AVG. DISTANCE</p>
+                <p className="subheading1 text-three mt-2">
+                  {selectedDestinationData?.distance.toUpperCase()}
+                </p>
+              </div>
+              <div className="my-8 md:my-0 md:mt-2">
+                <p className="subheading2 text-secondary">EST. TRAVEL TIME</p>
+                <p className="subheading1 text-three mt-2">
+                  {selectedDestinationData?.travel.toUpperCase()}
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
