6b510270cb475bafa95a8c18539fb69142946ffd
Seth Brown <sdbrown93@gmail.com>
2023-10-01 19:07:14 -0400

    Crew update

--- src/app/crew/page.tsx
+++ src/app/crew/page.tsx
@@ -27,30 +27,36 @@ export default function Crew() {
             YOUR CREW
           </h5>
         </div>
-        <div className="w-full md:flex md:flex-col-reverse md:h-screen md:justify-between md:max-h-[1024px] lg:flex-row-reverse lg:justify-evenly">
-          <div className="flex justify-center mt-10 ">
+        <div className="w-full md:flex md:flex-col-reverse lg:flex-row-reverse lg:justify-evenly">
+          <div className="flex justify-center mt-10">
             <Image
               src={selectedImageSource}
               alt="crew member"
               width={750}
               height={750}
-              className="h-56 w-auto md:h-[525px]"
+              className="h-56 w-auto md:h-[525px] md:absolute md:bottom-0 lg:h-[632px]"
             />
           </div>
-          <div className="md:flex md:flex-col-reverse lg:h-[420px] lg:w-[600px]">
+          <div className="md:flex md:flex-col-reverse lg:w-1/2 lg:justify-center">
             <div className="flex justify-center">
               <hr className="h-px w-10/12 bg-three/25 border-0 md:hidden" />
             </div>
-            <div className="my-8 flex justify-evenly w-40 m-auto">
-              {data.crew.map((crewMember, index) => (
-                <button
-                  key={index}
-                  className={`w-3.5 h-3.5 bg-three/25 rounded-full hover:bg-three/50 active:bg-three/100 focus:bg-three/100 ${
-                    index === crewIndex ? "bg-three/100" : "" // Apply active-button class if index matches
+            <div className="my-8 flex justify-evenly m-auto">
+              <ul className="button-list flex gap-4">
+                {data.crew.map((crewMember, index) => (
+                  <li key={index}>
+                    <button
+                      className={`w-3.5 h-3.5 rounded-full
+                  ${
+                    index === crewIndex
+                      ? "bg-three/100"
+                      : "bg-three/25 lg:hover:bg-three/50 active:bg-three/100 focus:bg-three/100"
                   }`}
-                  onClick={() => handleButtonClick(index)} // Set the click event to change crew index
-                ></button>
-              ))}
+                      onClick={() => handleButtonClick(index)}
+                    ></button>
+                  </li>
+                ))}
+              </ul>
             </div>
             <div className="flex flex-col md:mt-10">
               <h4 className="text-three/50 text-center md:my-2">
